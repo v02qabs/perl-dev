c83b8ab720e067b8754dbc8facaa6f84f67976e0
v02qabs <takesue090@gmail.com>
2024-01-14 22:44:46 +0900

    known-hash-array-list;

--- english-game.pl
+++ english-game.pl
@@ -1,17 +1,12 @@
-use strict;
-use warnings;
-
 print "If you start game when , push the Enter key. \n";
 my $Enter = <STDIN>;
+my $op;
 
-my %words = (a=>['私にそれをさせてください。', 'Let me do it.'], b=>['留学する事を決める', 'Decide to study abroad.']
-	, c=>['犬をきちんと訓練する','Train dogs properly.']
-	,d=>['日本中を旅行する', 'Travel all through Japan.'] 
-	,e=>['ドアを開けたままにする。', 'Leave a door open.']
-	,f=>['車を止める', 'Park a car.']
-	,g=>['見本', 'specimen']);
-
-open(my $op ,"<", "./mondai.txt");
-my @lines = map { chomp; $_ } <$op>;
+my $count;
+my $lines;
+my $c1;
+my $b1;
+open($op ,"<", "./mondai.txt");
+@lines = map { $_ } <$op>;
 
 close $op;
