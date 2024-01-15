f04ae29a301ed183d814186f3aff5c1f692dbf35
v02qabs <takesue090@gmail.com>
2024-01-15 09:25:05 +0900

    en-comp

--- english-game.pl
+++ english-game.pl
@@ -6,10 +6,34 @@ my $count;
 my $lines;
 my $c1;
 my $b1;
+my $answer;
 open($op ,"<", "./mondai.txt");
 @lines = map { $_ } <$op>;
 
 print "--grep---"."\n";
-print @lines;
 
+for($c1=0;$c1<=$#lines; $c1++)
+{
+	print "問題:".$lines[$c1++];
+
+	if($c1 % 2 != 0)
+	{
+		print "練習 ".$lines[$c1];
+		
+	}
+	$answer = <STDIN>;
+	chomp($answer);
+	print "c1 ".$lines[$c1];
+	chomp($lines[$c1]);
+	if($answer eq $lines[$c1]){
+		print "success\n";
+	}
+	else{
+		print "failed\n";
+	}
+		
+}
+
+
+	
 
